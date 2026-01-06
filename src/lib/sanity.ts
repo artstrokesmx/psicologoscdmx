c4e0d9ad86ddad207cc119c41914fda5cb2b0efd
Miguel Arturo García Miranda <psic.arturomiranda@gmail.com>
2026-01-06 10:45:05 -0600

    Test harcodeo

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -3,7 +3,7 @@ import type { BlogPostListItem, BlogPostDetail } from '@/lib/types';
 
 // 1. Inicialización del cliente
 export const sanityClient = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  projectId: '4hvlxjly',
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
   apiVersion: '2024-01-01',
   useCdn: process.env.NODE_ENV === 'production', // Solo CDN en producción
