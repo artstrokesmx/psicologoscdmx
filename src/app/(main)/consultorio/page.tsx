495250d9cd3141936a0b394be4c9b316ddd9d405
Miguel Arturo García Miranda <psic.arturomiranda@gmail.com>
2026-01-05 20:23:33 -0600

    Corrigiendo el tema del modulo en consultorio-page

--- src/app/(main)/consultorio/page.tsx
+++ src/app/(main)/consultorio/page.tsx
@@ -1,5 +1,7 @@
-const ConsultorioPage = () => {
-    return <div>ConsultorioPage</div>;
+import React from 'react';
+
+const ConsultorioPage: React.FC = () => {
+    return <div>Consultorio de Psicología en CDMX</div>;
 };
 
 export default ConsultorioPage;
